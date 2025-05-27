7a1d53c7951bb72ce0d43a8c7109b067efbbb2b0
boomish2005 <sathiv289@gmail.com>
2025-05-28 00:14:15 +0530

    html

---
+++ JAVA/Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class Calculator {
+    public static void main(String[] args) {
+        String o;
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("1. +\n2. -\n3. *\n4. /");
+        int c=sc.nextInt();
+        int r=0;
+        switch(c)
+        {
+            case 1:
+                r=a+b;
+                o="+";
+                break;
+            case 2:
+                r=a-b;
+                o="-";
+                break;
+            case 3:
+                r=a*b;
+                o="*";
+                break;
+            case 4:
+                if(b==0)
+                {
+                    System.out.println("Error");
+                }
+                r=a/b;
+                o= "/";
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+        System.out.println(r);
+        sc.close(); 
+    }
+}
