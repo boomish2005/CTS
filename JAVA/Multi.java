ed5da905705b9d8cdb39611bae2dc43b5f854285
boomish2005 <sathiv289@gmail.com>
2025-05-28 00:26:30 +0530

    ht

---
+++ JAVA/Multi.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Multi {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int j=10;
+        for(int i=1;i<=n;i++)
+        {
+            System.err.println("table"+i*j);
+        }
+    }
+}
