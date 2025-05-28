e08e73d4dc5d4435de1aed583662cbb2815d2c23
boomish2005 <sathiv289@gmail.com>
2025-05-28 22:10:21 +0530

    ht

---
+++ JAVA/Classcreation_Objectcreation.java
@@ -0,0 +1,24 @@
+public class Classcreation_Objectcreation {
+    public class Car
+
+{
+    String make;
+    String model;
+    int year;
+    public Car(String make,String model,int year)
+    {
+        this.make=make;
+        this.model=model;
+        this.year=year;
+    }
+    public  void display()
+    {
+        System.out.println(year +" " + make +" "+ " " +model);
+    }
+}    public static void main(String[] args) {
+    Car a=new Car("tvs","xl",2010);
+    Car b=new Car("xl","xl",2018);
+    a.display();
+    b.display();
+} 
+}
