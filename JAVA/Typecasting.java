01082e8848dd90fae871ceffd796c5cf13af5b8d
boomish2005 <sathiv289@gmail.com>
2025-05-28 21:55:36 +0530

    ht

---
+++ JAVA/Typecasting.java
@@ -0,0 +1,10 @@
+public class Typecasting {
+    public static void main(String[] args) {
+        double a=1.0d;
+        int b=(int)a;
+        System.out.println(b);
+        int c=10;
+        double w=(double)c;
+        System.out.println(w);
+    }
+}
