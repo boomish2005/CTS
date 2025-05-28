369210e64314ad04a718ec0362635007dd2854f7
boomish2005 <sathiv289@gmail.com>
2025-05-28 21:52:50 +0530

    ht

---
+++ JAVA/Datatypes.java
@@ -0,0 +1,14 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int a=10;
+        double c=4.0d;
+        float b=20.0f;
+        char d='h';
+        boolean f=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+    }
+}
